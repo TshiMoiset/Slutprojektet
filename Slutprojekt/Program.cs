f7bf6333a87e61c685b280771f0d4a0afabc04d4
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-05-07 12:09:58 +0200

    Update Program.cs

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -6,11 +6,21 @@ namespace Slutprojekt
     {
         static void Main(string[] args)
         {
-            int[,] layout = new int[20, 20]; // Jag har en 2 dimensionell array (grid).
+            Console.WriteLine("Välkomen till 21:an");
+
+            string menuChoises = "0";
+
+            while (menuChoises != "3") // Innehållet som finns i menyn.
+            {
+                Console.WriteLine("Välj ett alternativ");
+                Console.WriteLine("1. Spela 21:an");
+                Console.WriteLine("2. Spelets regler");
+                Console.WriteLine("3. Avsluta programmet");
+                menuChoises = Console.ReadLine();
+
+            }
 
             Console.ReadLine();
-
-
         }
     }
 }
