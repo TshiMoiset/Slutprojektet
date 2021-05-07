5e967245d30bf74a6a60179e99c46ae09700abfa
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-05-07 12:27:34 +0200

    :100:

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -6,18 +6,34 @@ namespace Slutprojekt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Välkomen till 21:an");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Välkomen till 21:an :)");
 
-            string menuChoises = "0";
+            int menuChoises = 0;
 
-            while (menuChoises != "3") // Innehållet som finns i menyn.
+            string menuChoisesString = "";
+
+
+            while (menuChoises != 3) // Innehållet som finns i menyn.
             {
+                Console.WriteLine();
                 Console.WriteLine("Välj ett alternativ");
                 Console.WriteLine("1. Spela 21:an");
                 Console.WriteLine("2. Spelets regler");
                 Console.WriteLine("3. Avsluta programmet");
-                menuChoises = Console.ReadLine();
+                menuChoisesString = Console.ReadLine();
 
+                while (!int.TryParse(menuChoisesString, out menuChoises))       // Koden gör att den inte krashar om anvädaren inte följer instruktionerna.
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Det där är inte ett giltigt svar. Försök igen!");
+
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write("Ok, Jag väljer då: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    menuChoisesString = Console.ReadLine();
+                }
             }
 
             Console.ReadLine();
